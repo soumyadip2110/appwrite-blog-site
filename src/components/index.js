aa890d599dc7b2fe4b2c7d0713ee54d953b6ed24
Soumyadip Chowdhury <soumyadip2110@gmail.com>
2024-08-12 23:50:35 +0530

    setup real time editor and post form

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,6 @@ import LogoutBtn from './Header/LogoutBtn'
 import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select }
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select, RTE }
