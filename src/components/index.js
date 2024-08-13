14ae51cee83b61a2d5e8628851a31107514d35a9
Soumyadip Chowdhury <soumyadip2110@gmail.com>
2024-08-13 13:30:05 +0530

    add pages components

--- src/components/index.js
+++ src/components/index.js
@@ -7,5 +7,10 @@ import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
 import RTE from "./RTE";
+import Signup from './Signup'
+import Login from './Login'
+import PostForm from './post-form/PostForm'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select, RTE }
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select, RTE, Signup, Login, PostForm, PostCard, AuthLayout }
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from './Home'
+import AddPost from './AddPost';
+import Signup from './Signup'
+import Login from './Login'
+import EditPost from "./EditPost";
+import Post from "./Post";
+import AllPosts from "./AllPosts";
+
+export {Home, AddPost, Signup, Login, EditPost, Post, AllPosts}
